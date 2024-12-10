13f61e322798c36f555abd6cb9c426526a5c5eca
Volkan Molla <mollavolkan11@gmail.com>
2024-12-10 21:36:56 +0300

    Refactor MailMe component for improved clarity and functionality
    - Added comments throughout the MailMe component to enhance code readability and maintainability.

--- app/components/MailMe/MailMe.jsx
+++ app/components/MailMe/MailMe.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import "@/app/components/MailMe/style.css";
 import React, { useEffect, useRef, useState } from "react";
 import { useAppContext } from "@/app/context/AppContext";
@@ -9,31 +10,36 @@ import { sendEmail } from "@/app/components/utils/sendMail";
 import DateComponent from "./DateComponent";
 
 export default function MailMe() {
-  const { itim, toggleClasses } = useAppContext();
-  const textareaRef = useRef(null);
-  const formRef = useRef(null);
-  const [status, setStatus] = useState(null);
+  const { itim, toggleClasses } = useAppContext(); // Get context values
+  const textareaRef = useRef(null); // Reference to the textarea element
+  const formRef = useRef(null); // Reference to the form element
+  const [status, setStatus] = useState(null); // Track the status of the form submission
 
+  // Handle form submission
   async function handleSubmit(event) {
     event.preventDefault();
     setStatus("sending");
-    const formData = new FormData(event.target);
-    const result = await sendEmail(formData);
+
+    const formData = new FormData(event.target); // Collect form data
+    const result = await sendEmail(formData); // Send email
 
     if (result.success) {
-      setStatus("success");
+      setStatus("success"); // Update status on successful email sending
       setTimeout(() => {
-        event.target.reset();
-        setStatus(null);
+        event.target.reset(); // Reset form after a delay
+        setStatus(null); // Clear status
       }, 3000);
     }
   }
+
+  // Trigger animations when the status changes
   useEffect(() => {
     AnimateEnvelope({ status, toggleClasses, PlaySound });
   }, [status]);
 
   return (
     <div className="size-full flex justify-center items-center overflow-hidden">
+      {/* Success/Error message display */}
       <div className="success absolute opacity-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-2xl transition-all duration-500">
         {status && (
           <p
@@ -45,19 +51,23 @@ export default function MailMe() {
           </p>
         )}
       </div>
-      <div className="envelope-container w-11/12 sm:w-2/3 md:w-2/4  lg:w-2/6 translate-x-0 h-1/2 mt-3 flex opacity-100 justify-center items-center  transition-all duration-500">
+
+      {/* Envelope Container */}
+      <div className="envelope-container w-11/12 sm:w-2/3 md:w-2/4 lg:w-2/6 translate-x-0 h-1/2 mt-3 flex opacity-100 justify-center items-center transition-all duration-500">
         <div
-          className={`envelope size-full flex   justify-center items-center transition-all duration-500 translate-y-3/4 `}
+          className={`envelope size-full flex justify-center items-center transition-all duration-500 translate-y-3/4`}
         >
-          <div className="size-full bg-zinc-500 shadow-inner  shadow-black transition-all duration-500">
+          {/* Envelope Design */}
+          <div className="size-full bg-zinc-500 shadow-inner shadow-black transition-all duration-500">
             <form
               onSubmit={handleSubmit}
               ref={formRef}
-              className={`paper left-1/2 -translate-x-1/2 -translate-y-[105%]  absolute h-[95%]  mt-2 w-[95%] bg-zinc-100 text-black shadow-md shadow-black border border-dashed border-red-900 rounded transition-all duration-500 ${itim.className}`}
+              className={`paper left-1/2 -translate-x-1/2 -translate-y-[105%] absolute h-[95%] mt-2 w-[95%] bg-zinc-100 text-black shadow-md shadow-black border border-dashed border-red-900 rounded transition-all duration-500 ${itim.className}`}
             >
-              <div className="w-full h-[90%]  flex flex-col relative p-5 tracking-wide">
+              {/* Form Inputs */}
+              <div className="w-full h-[90%] flex flex-col relative p-5 tracking-wide">
                 {/* Message input */}
-                <div className="size-full  flex justify-between items-center relative">
+                <div className="size-full flex justify-between items-center relative">
                   <textarea
                     ref={textareaRef}
                     id="message"
@@ -73,8 +83,9 @@ export default function MailMe() {
                     placeholder="..."
                   ></textarea>
                 </div>
-                {/* Email and Name inputs  */}
-                <div className="h-1/3 flex gap-5 md:gap-10 text-sm  justify-between items-center">
+
+                {/* Email and Name inputs */}
+                <div className="h-1/3 flex gap-5 md:gap-10 text-sm justify-between items-center">
                   <div className="w-1/2">
                     <label htmlFor="name">Name:</label>
                     <input
@@ -95,7 +106,6 @@ export default function MailMe() {
                   </div>
                   <div className="w-1/2">
                     <label htmlFor="email">Email:</label>
-
                     <input
                       type="email"
                       id="email"
@@ -114,29 +124,32 @@ export default function MailMe() {
                   </div>
                 </div>
               </div>
+
+              {/* Date Component */}
               <span className="absolute top-1 right-2 text-red-900">
                 <DateComponent />
               </span>
             </form>
+
+            {/* Envelope Animation */}
             <div
-              className={`top-triangle w-full h-full absolute open -z-50 -top-full bg-zinc-700 clip-triangle3 origin-bottom ease-in-out preserve3d transition-all duration-500
-              `}
+              className={`top-triangle w-full h-full absolute open -z-50 -top-full bg-zinc-700 clip-triangle3 origin-bottom ease-in-out preserve3d transition-all duration-500`}
             ></div>
-            <div className="w-1/2 h-full absolute bg-zinc-700  clip-triangle "></div>
-            <div className="w-1/2 h-full absolute translate-x-full rotate-180 bg-zinc-700  clip-triangle "></div>
-            <div className="w-full h-full  absolute bg-zinc-800 clip-triangle2"></div>
-            <div className=" size-max absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50">
+            <div className="w-1/2 h-full absolute bg-zinc-700 clip-triangle "></div>
+            <div className="w-1/2 h-full absolute translate-x-full rotate-180 bg-zinc-700 clip-triangle "></div>
+            <div className="w-full h-full absolute bg-zinc-800 clip-triangle2"></div>
+
+            {/* Submit Button */}
+            <div className="size-max absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50">
               <button
                 type="submit"
                 onClick={() => formRef.current.requestSubmit()}
                 disabled={status === "success"}
-                className={`hover:scale-[70%] scale-[65%] w-32 h-32 rounded-full border-none outline-none
-                  shadow-none hover:shadow-[0_0_20px_rgba(0,0,0,1)]
-                     transition-all duration-200 ${
-                       status === "sending"
-                         ? "spin shadow-none"
-                         : " shadow-[0_0_20px_rgba(0,0,0,1)]"
-                     }`}
+                className={`hover:scale-[70%] scale-[65%] w-32 h-32 rounded-full border-none outline-none shadow-none hover:shadow-[0_0_20px_rgba(0,0,0,1)] transition-all duration-200 ${
+                  status === "sending"
+                    ? "spin shadow-none"
+                    : " shadow-[0_0_20px_rgba(0,0,0,1)]"
+                }`}
               >
                 <SealIcon />
               </button>
