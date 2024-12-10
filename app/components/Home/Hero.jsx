b19591ceaf3f0902d50bcb4014ae968146700f33
Volkan Molla <mollavolkan11@gmail.com>
2024-12-10 20:37:21 +0300

    fix buttons

--- app/components/Home/Hero.jsx
+++ app/components/Home/Hero.jsx
@@ -56,7 +56,7 @@ const Hero = () => {
           className="bg-zinc-800 text-nowrap w-36 md:w-48 text-red-300 px-4 py-2 rounded-full hover:bg-zinc-700 transition-colors duration-300 flex items-center justify-center gap-2"
           target="_blank"
         >
-          <Github />
+          <Github className=" size-4 md:size-5" />
           <span>Github</span>
         </a>
       </div>
