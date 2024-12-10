0ed6bb0532717c0cf59a4666848246ea518756c6
Volkan Molla <mollavolkan11@gmail.com>
2024-12-10 21:34:29 +0300

    Enhance DateComponent with comments and improved date formatting

--- app/components/MailMe/DateComponent.jsx
+++ app/components/MailMe/DateComponent.jsx
@@ -1,9 +1,15 @@
 "use client";
-import React from "react";
-const DateComponent = () => {
-  const date = new Intl.DateTimeFormat("tr-TR").format(new Date());
 
-  return <div>{date}</div>;
+import React from "react";
+
+const DateComponent = () => {
+  // Get the current date formatted in Turkish (tr-TR) locale
+  const formattedDate = new Intl.DateTimeFormat("tr-TR").format(new Date());
+
+  return (
+    // Render the formatted date inside a div
+    <div>{formattedDate}</div>
+  );
 };
 
 export default DateComponent;
