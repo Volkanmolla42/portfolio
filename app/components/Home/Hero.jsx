2c7aea51f93e8a8030ad639cddf3cc8237a0c54a
Volkan Molla <mollavolkan11@gmail.com>
2024-12-10 20:34:30 +0300

    fix text overflow

--- app/components/Home/Hero.jsx
+++ app/components/Home/Hero.jsx
@@ -46,15 +46,14 @@ const Hero = () => {
         <a
           href="https://wa.me/905418224484"
           target="_blank"
-          className="bg-zinc-800 w-36 md:w-48 text-red-300 px-4 py-2 rounded-full hover:bg-zinc-700 transition-colors duration-300 flex items-center justify-center gap-2"
+          className="bg-zinc-800 text-nowrap w-36 md:w-48 text-red-300 px-4 py-2 rounded-full hover:bg-zinc-700 transition-colors duration-300 flex items-center justify-center gap-2"
         >
-          <div className="border-none rounded-full p-[6px] bg-green-500 mr-2 animate-pulse"></div>
-          <SiWhatsapp size={18} />
-          <span>Let&apos; s Talk</span>
+          <SiWhatsapp className=" size-4 md:size-5 fill-green-600 animate-pulse" />
+          <span>Get in Touch</span>
         </a>
         <a
           href="https://github.com/Volkanmolla42"
-          className="bg-zinc-800 w-36 md:w-48 text-red-300 px-4 py-2 rounded-full hover:bg-zinc-700 transition-colors duration-300 flex items-center justify-center gap-2"
+          className="bg-zinc-800 text-nowrap w-36 md:w-48 text-red-300 px-4 py-2 rounded-full hover:bg-zinc-700 transition-colors duration-300 flex items-center justify-center gap-2"
           target="_blank"
         >
           <Github />
