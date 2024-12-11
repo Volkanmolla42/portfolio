8ed5cf447a309163c7b45d25a2ba034df6ee9110
Volkan Molla <mollavolkan11@gmail.com>
2024-12-11 20:37:33 +0300

    Refactor Section component for improved class management and prop validation
    - Introduced useMemo for optimized className calculation based on currentHash and id.
    - Replaced inline className logic with clsx for better readability and maintainability.
    - Added PropTypes for children and id to enforce prop validation.

--- app/components/Section.jsx
+++ app/components/Section.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useAppContext } from "@/app/context/AppContext";
+import clsx from "clsx"; // Using clsx for className management
+
 const Section = ({ children, id }) => {
   const { currentHash } = useAppContext();
+
+  // useMemo ensures the className is recalculated only when currentHash or id changes
+  const sectionClassName = useMemo(
+    () =>
+      clsx(
+        "size-full flex flex-col items-center justify-center gap-3 absolute inset-0 transition-opacity duration-500 bg-transparent overflow-y-auto overflow-x-hidden scrollbar-thin scrollbar-thumb-red-700 scrollbar-track-gray-800 scroll-smooth",
+        {
+          "opacity-100 z-50": currentHash === `#${id}`,
+          "opacity-0 -z-50": currentHash !== `#${id}`,
+        }
+      ),
+    [currentHash, id]
+  ); // Dependencies: currentHash and id
+
   return (
     <section
       id={id}
       aria-labelledby={`${id}-heading`}
-      className={` size-full flex flex-col items-center justify-center gap-3 absolute inset-0 transition-opacity duration-500 bg-transparent overflow-y-auto overflow-x-hidden scrollbar-thin scrollbar-thumb-red-700 scrollbar-track-gray-800 scroll-smooth  ${
-        currentHash === `#${id}` ? "opacity-100 z-50 " : "opacity-0 -z-50 "
-      }`}
+      className={sectionClassName}
     >
       {children}
     </section>
