ecf00e6704e7e12d7fe84aafd305749383ff607b
Volkan Molla <mollavolkan11@gmail.com>
2024-12-12 20:32:19 +0300

    Refactor Hero component for improved styling and layout
    - Updated ContactButton className for better alignment and responsiveness.
    - Adjusted gap and width properties to enhance visual consistency.
    - Removed the ExternalLink icon for a cleaner design.

--- app/components/Home/Hero.jsx
+++ app/components/Home/Hero.jsx
@@ -86,11 +86,10 @@ const ContactButton = ({ href, icon, text, className }) => (
     href={href}
     target="_blank"
     rel="noopener noreferrer"
-    className="flex items-center text-sm justify-center gap-2 bg-zinc-800 text-nowrap w-36 md:w-48 text-red-300 px-4 py-2 rounded-full hover:bg-zinc-700 transition-colors duration-300"
+    className="flex items-center text-sm justify-center gap-1 bg-zinc-800 text-nowrap w-max text-red-300 px-4 py-2 rounded-full hover:bg-zinc-700 transition-colors duration-300 hover:underline hover:underline-offset-4"
   >
     <img src={icon} alt="contact icon" className={className}></img>
     <span>{text}</span>
-    <ExternalLink size={16} />
   </a>
 );
 
